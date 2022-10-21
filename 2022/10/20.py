81cfededccdf23106514057525573af18fc1a67c
Trung Dang Nguyen <trung.dang@landbot.io>
2022-10-21 17:01:12 +0200

    Add another solution

--- 2022/10/20.py
+++ 2022/10/20.py
@@ -4,22 +4,29 @@
 # Given a list of numbers and a number `k`, return whether any two numbers from the list add up to `k`.
 # Bonus: Do it in one line.
 
-def read_input(filename):
-    with open(filename) as f:
-        values = [int(line) for line in f.readlines()]
-    return values
 
+## Solution 1
 
-def check_addition(lst: list, k: int) -> list:
-    for i in range(len(lst)):
-        for j in range(len(lst)):
-            if (not i >= j) and lst[i] + lst[j] == k:
-                return [lst[i], lst[j]]
+def check_addition(lst: list, k: int) -> bool:
+    for i in lst:
+        if k - i in lst:
+            return True
+    return False
+
+# result = any(k - i in lst for i in lst)
 
 if __name__ == '__main__':
+    values = [10, 15, 3, 7]
     k = 17
-    values = read_input('inputs/20.txt')
     print(check_addition(values, k))
 
-# Bonus:
+
+## Solution 2
+
+# def check_addition(lst: list, k: int) -> list:
+#     for i in range(len(lst)):
+#         for j in range(len(lst)):
+#             if (not i >= j) and lst[i] + lst[j] == k:
+#                 return [lst[i], lst[j]]
+
 # result = [(lst[i], lst[j]) for i in range(len(lst)) for j in range(len(lst)) if ((not i >= j) and (lst[i] + lst[j] == k))]
